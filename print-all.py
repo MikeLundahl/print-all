2d90b8c02b684593ee5bf44ab0035398b095e873
Mike Lundahl <mikeclundahl@gmail.com>
2021-05-14 23:45:55 +0200

    first commit

---
+++ print-all.py
@@ -0,0 +1,21 @@
+#import os system
+
+base_folder = raw_input("Base folder\n") #use input() for python 3
+all_folders = [base_folder, "folder 2", "folder 3"]
+print_jobs_all = None
+print_jobs_devided = [[1,2,3,4,5], [1,2,3,4,5]] # each list max 15
+
+print "the base is: ", base_folder #use () for python 3
+
+#add to print queue
+    #add first 15
+    #when done, next 15
+
+#scan for folders
+    #add folder to all folders
+
+#scan folders for files
+    #add files to print jobs all
+
+#split jobs
+    #split to 15 max list
